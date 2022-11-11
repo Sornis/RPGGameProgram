393263e6dcd6a05934893a32ae16e989827fb081
kasper kammer <88823753+foulslayer@users.noreply.github.com>
2022-11-11 09:26:07 +0100

    fil skrivning
    vi har ikke en Console i WPF så  vi har lave side program med fil skrivning og læsning

---
+++ læse og skirve i file/læse og skirve i file/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace læseOgSkirveIFil
+{
+
+    class program
+    {
+        static void Main(String[] args)
+        {
+
+            // den overwrite hele vores fil og slette alt text der stod før i vores fil 
+
+            if (File.Exists("C:\\Users\\kaspe\\OneDrive\\Skrivebord\\pepsi.txt")) // hvis vi ikke havde denne line code og vores file ikke findes så ville den bare laver en ny 
+            {
+                string hvad_Vi_skrive_i_vores_file = "det virker"; // hvad vi havde skal står i vores file 
+
+                File.WriteAllText("C:\\Users\\kaspe\\OneDrive\\Skrivebord\\pepsi.txt", hvad_Vi_skrive_i_vores_file); // her skrive i vores fil 
+            }
+
+
+
+            if (File.Exists("C:\\Users\\kaspe\\OneDrive\\Skrivebord\\pepsi.txt"))
+            {
+                string pepsi = File.ReadAllText("C:\\Users\\kaspe\\OneDrive\\Skrivebord\\pepsi.txt"); // vi gør at vores file bliv taget som en Data type string 
+                Console.WriteLine(pepsi);
+            }
+
+
+
+
+
+
+        }
+    }
+}
